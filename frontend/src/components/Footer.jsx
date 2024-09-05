c8d191da3c2a1d5de3f44dc60f18783b9d85a84f
Zaki3584 <zakihamidi385@gmail.com>
2024-09-05 14:41:54 +0100

    j
    rg53
    
    Co-Authored-By: bentafat wail <150479778+wailbentafat@users.noreply.github.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -14,10 +15,10 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>COMPANY</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
+                    <li><Link to="/">Home</Link> </li>
+                    <li><Link to="/about">About us </Link></li>
+                    <li><Link to="/">Delivery</Link></li>
+                    <li><Link to="/">Privacy policy</Link></li>
             </ul>
         </div>
         <div>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom"
  const ShopContextProvider = (props) =>{
 
     const currency = ' DA'
-    const delivery_fee =10;
+    const [delivery_fee , setDelivery_fee] = useState(0);
     const [search ,setSearch]=useState('');
     const[showSearch ,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({})
@@ -82,7 +82,7 @@ import { useNavigate } from "react-router-dom"
 
 
     const value ={
-        products, currency ,delivery_fee,
+        products, currency ,delivery_fee,setDelivery_fee,
         search ,setSearch ,showSearch ,setShowSearch,cartItems,
         addToCart,getCartCount,updateQuantity,getCartAmount,navigate
     }
