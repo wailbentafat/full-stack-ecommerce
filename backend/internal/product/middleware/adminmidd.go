b93f5299d606d7e82679e84f5c6ade013aa5662c
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2024-09-08 22:32:28 +0100

    finish product

--- backend/internal/product/middleware/adminmidd.go
+++ backend/internal/product/middleware/adminmidd.go
@@ -1,1 +1,52 @@
-package admin
+package adminpackage
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+
+	 m"github.com/wailbentafat/full-stack-ecommerce/backend/internal/auth/jwt"
+)
+var admin bool
+
+func AdminMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+
+		
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
+			c.Abort()
+			return
+		}
+       
+		
+		tokenStr := parts[1]
+		claims, err := m.Parsejwt(tokenStr)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+        if claims=="www.hamidbentafat@gmail.com"{
+			admin=true
+		}else{
+			admin=false
+		}
+		
+		if !admin {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "You are not an admin"})
+			c.Abort()
+			return
+		}
+		
+		c.Next()
+	}
+}
--- backend/internal/product/routes/routes.go
+++ backend/internal/product/routes/routes.go
@@ -4,12 +4,13 @@ package product_routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wailbentafat/full-stack-ecommerce/backend/internal/product/provider"
+	a"github.com/wailbentafat/full-stack-ecommerce/backend/internal/product/middleware"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/products", product.GetAllProduct)
+	router.GET("/products",product.GetAllProduct)
 	router.GET("/product/:id", product.GetProduct)
-	router.POST("/product", product.CreateProduct)
-	router.PUT("/product/:id", product.UpdateProduct)
-	router.DELETE("/product/:id", product.DeleteProduct)
+	router.POST("/product",a.AdminMiddleware(), product.CreateProduct)
+	router.PUT("/product/:id", a.AdminMiddleware(),product.UpdateProduct)
+	router.DELETE("/product/:id",a.AdminMiddleware(), product.DeleteProduct)
 }
